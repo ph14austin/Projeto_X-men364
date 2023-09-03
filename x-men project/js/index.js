5251ea732586bc42454ba761e794c52d2a991725
ph14austin <143339491+ph14austin@users.noreply.github.com>
2023-09-03 17:41:32 -0300

    Add files via upload

---
+++ x-men project/js/index.js
@@ -0,0 +1,38 @@
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        if (window.innerWidth < 450) {
+
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+        }
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+
+        removerSelecaoDoPersonagem(personagemSelecionado, personagem);
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande')
+
+        const idPersonagem = personagem.attributes.id.value;
+
+        imagemPersonagemGrande.src = `./css/src/card-${idPersonagem}.png`;
+
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description')
+
+
+
+
+    })
+})
+
+function removerSelecaoDoPersonagem(personagemSelecionado, personagem) {
+    personagemSelecionado.classList.remove('selecionado');
+
+    personagem.classList.add('selecionado');
+}
